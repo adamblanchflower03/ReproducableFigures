4c77fb2c2bfac8c4b7b0a1b103089ef6b06451f5
adamblanchflower03 <sann7568@ox.ac.uk>
2024-12-02 16:35:01 +0000

    changing save function

--- functions/png_and_svg_functions.R
+++ functions/png_and_svg_functions.R
@@ -22,7 +22,7 @@ save_as_png <- function(data, filename, plot, size, res, scaling){
 
 #save as .svg
 
-save_as_svg <- function(data, filename, plot, size, res, scaling){
+save_as_svg <- function(data, filename, plot, size, scaling){
   size_inches = size/2.54
   svglite(filename, width = size_inches,
           height = size_inches,
@@ -35,17 +35,17 @@ save_as_svg <- function(data, filename, plot, size, res, scaling){
 # Function to diagnostic plot as .png
 save_diagnostic_plot_as_png <- function(filename, plot_func, size, res, scaling) {
   agg_png(filename, width = size, height = size, units = "cm", res = res, scaling = scaling)
-  par(mfrow = c(2, 2), mar = c(4, 4, 2, 2))  # Set layout to 2x2 grid and margins
-  plot_func()  # Call the function to create the plot
-  dev.off()  # Close the graphics device
+  par(mfrow = c(2, 2), mar = c(4, 4, 2, 2))  
+  plot_func()  
+  dev.off() 
 }
 
 # Function to save diagnostic plot as .svg
 save_diagnostic_plot_as_svg <- function(filename, plot_func, size, scaling) {
-  size_inches <- size / 2.54  # Convert size from cm to inches
+  size_inches <- size / 2.54  
   svglite(filename, width = size_inches, height = size_inches, scaling = scaling)
-  par(mfrow = c(2, 2), mar = c(4, 4, 2, 2))  # Set layout to 2x2 grid and margins
-  plot_func()  # Call the function to create the plot
-  dev.off()  # Close the graphics device
+  par(mfrow = c(2, 2), mar = c(4, 4, 2, 2))  
+  plot_func()  
+  dev.off()  
 }
 
