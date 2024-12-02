ff599619784e11c31c0186265de33d9b93c95707
adamblanchflower03 <sann7568@ox.ac.uk>
2024-12-02 12:31:17 +0000

    Polishing off code

--- functions/diagnostic_plot_function.R
+++ functions/diagnostic_plot_function.R
@@ -1,3 +1,13 @@
+## Script name: diagnostic_plot_function.R
+##
+## Purpose of script: 
+##      # A function for produciung diagnostic plots 
+##
+## Author: Adam Blanchflower
+##
+## Date Created: 02-12-2024
+##
+
 diagnostic_plot_function <- function() {
   plot(linear_model)  
 }
--- functions/linear_model_figure.R
+++ functions/linear_model_figure.R
@@ -1,3 +1,13 @@
+## Script name: linear_model_figure.R
+##
+## Purpose of script: 
+##      # A function for producing linear model figures  
+##
+## Author: Adam Blanchflower
+##
+## Date Created: 02-12-2024
+##
+
 linear_model_figure <- function(data, x_column, y_column, x_label, y_label) {
   ggplot(data = data, 
          aes(x = {{ x_column }},
